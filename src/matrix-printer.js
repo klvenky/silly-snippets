61e82a2f78f714c58b00038883759a9d2302ef1a
klvenky <venkatesh@convertcart.com>
2020-02-11 15:40:41 +0530

    added matrix printer

---
+++ src/matrix-printer.js
@@ -0,0 +1,16 @@
+module.exports = function matrixPrinter(n) {
+  if (!n % 2) {
+    throw new Error('only +ve odd numbers are expected');
+  }
+  let str = '';
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      if (i === 0 || j === 0 || i === n - 1 || j === n - 1 || i === j || i + j === n - 1)
+        str += '0';
+      else str += '1';
+      str += ' ';
+    }
+    str += '\n';
+  }
+  return str;
+};
