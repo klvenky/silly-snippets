f0dc4d86a5fd0f9fba9508c26c994af97fc86dd0
klvenky <k.leelavenkatesh@gmail.com>
2020-08-18 15:32:28 +0530

    changed the matrix printer a little.

--- src/matrix-printer.js
+++ src/matrix-printer.js
@@ -1,4 +1,4 @@
-module.exports = function matrixPrinter(n) {
+function matrixPrinter(n) {
   if (!n % 2) {
     throw new Error('only +ve odd numbers are expected');
   }
@@ -6,11 +6,12 @@ module.exports = function matrixPrinter(n) {
   for (let i = 0; i < n; i += 1) {
     for (let j = 0; j < n; j += 1) {
       if (i === 0 || j === 0 || i === n - 1 || j === n - 1 || i === j || i + j === n - 1)
-        str += '0';
-      else str += '1';
+        str += '*';
+      else str += ' ';
       str += ' ';
     }
     str += '\n';
   }
-  return str;
+  console.log(str);
 };
+matrixPrinter(11);
